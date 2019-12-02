c6edfb2dd06a41540570700a5ad89f095f1e36b7
Andreas Kohn <andreas.kohn@gmail.com>
2019-12-02 18:31:00 +0100

    WIP: Fix the type of the 'grants' property of Client
    Grants are always assumed to be an array, so define them as such here.

--- lib/interfaces/client.interface.ts
+++ lib/interfaces/client.interface.ts
@@ -4,7 +4,7 @@
 export interface Client {
   id: string;
   redirectUris?: string | string[];
-  grants: string | string[];
+  grants: string[];
   accessTokenLifetime?: number;
   refreshTokenLifetime?: number;
   [key: string]: any;
